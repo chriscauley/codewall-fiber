16ec0015b564fbb2956dfc715fa6b2a8cf9533c8
chriscauley <chris@lablackey.com>
2021-08-04 12:37:46 +0000

    building

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/codewall-fiber/',
   plugins: [vue(), svgLoader()],
+  build: {
+    outDir: path.resolve(__dirname, 'docs'),
+  },
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`
