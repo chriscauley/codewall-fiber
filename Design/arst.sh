c08f2bb8cc2dd7d5f1c3122abf0c3e255de99378
chriscauley <chris@lablackey.com>
2021-08-04 11:12:40 +0000

    resizing design images

---
+++ Design/arst.sh
@@ -0,0 +1,4 @@
+convert Landing\ Page\ -\ Desktop\ View.png -resize 50% landing_desktop.png
+convert Landing\ Page\ -\ Mobile\ View.png -resize 50% landing_mobile.png
+convert Sign\ Up\ Page\ -\ Desktop\ View.png -resize 50% sign_up_desktop.png
+convert Sign\ Up\ Page\ -\ Mobile\ View.png -resize 50% sign_up_mobile.png
